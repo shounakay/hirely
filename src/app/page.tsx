b5e5bf53286c225e543b6123a76a32dc5cfad0ac
Shounak Chavan <chavanshounak24@gmail.com>
2023-12-10 17:17:13 +0530

    feat: Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="w-[500px] bg-red-300">
+      <div className=" w-full border-4 border-blue-400 p-[4px]">
+        Hello world
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
