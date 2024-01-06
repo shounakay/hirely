84639e7b0161e1860d4b48b01ef36c8a567f9cc0
Shounak Chavan <chavanshounak24@gmail.com>
2024-01-06 18:43:49 +0530

    feat: Restructured the footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,17 +1,63 @@
+import Image from "next/image";
 import Link from "next/link";
+import hirelyLogo from "../../../public/hirely-main.png";
+import { MdCopyright } from "react-icons/md";
 
 export const Footer = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       {children}
-      <footer className="mt-auto flex w-full items-center justify-between bg-slate-700 p-14 px-12 py-6">
-        <Link href="/">Hirely</Link>
-        <div className="flex h-auto w-32 flex-wrap justify-between px-4">
-          <Link href="/jobs">Jobs</Link>
-          <Link href="/jobs">Jobs</Link>
-          <Link href="/jobs">Jobs</Link>
-          <Link href="/jobs">Jobs</Link>
+      <footer className="mt-auto flex w-full flex-col items-center justify-center gap-7 bg-neutral-800 p-14 px-12 py-8">
+        <div className="flex basis-1/4 flex-col gap-1">
+          <Image
+            src={hirelyLogo}
+            alt="hirely"
+            width={50}
+            height={50}
+            className="self-center"
+          />
+          <h4 className="self-center font-mono text-lg font-semibold text-neutral-200">
+            Hirely
+          </h4>
+          <p className="w-96 text-justify font-sans text-base font-medium text-neutral-400">
+            Great platform for job seeker that is passionate about start ups.
+            Find your dream job with ease.
+          </p>
         </div>
+        <div className="flex h-auto w-[700px] flex-row flex-wrap justify-around px-4">
+          <Link
+            href="/"
+            className="hover:text-tarawera-600 hover:border-tarawera-600 border-b-[1px] border-neutral-200 text-neutral-200 transition-all"
+          >
+            Home
+          </Link>
+          <Link
+            className="hover:text-tarawera-600 hover:border-tarawera-600 border-b-[1px] border-neutral-200 text-neutral-200 transition-all"
+            href="/jobs"
+          >
+            All Jobs
+          </Link>
+          <Link
+            className="hover:text-tarawera-600 hover:border-tarawera-600 border-b-[1px] border-neutral-200 text-neutral-200 transition-all"
+            href="/auth/signin"
+          >
+            Sign In
+          </Link>
+          <Link
+            className="hover:text-tarawera-600 hover:border-tarawera-600 border-b-[1px] border-neutral-200 text-neutral-200 transition-all"
+            href="/auth/signup"
+          >
+            Sign Up
+          </Link>
+        </div>
+        <div className="w-[800px] border-[1px] border-neutral-400" />
+        <h4 className="flex items-center gap-1 font-mono text-base font-light text-neutral-400">
+          2024
+          <span>
+            <MdCopyright />
+          </span>
+          Shounak Chavan
+        </h4>
       </footer>
     </>
   );
